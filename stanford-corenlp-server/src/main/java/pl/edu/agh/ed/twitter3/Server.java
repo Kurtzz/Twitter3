ca4389f3975628b8200644d47a744d60938c0aab
Kurtzz <przemyslaw.kurc@gmail.com>
2017-10-26 20:36:48 +0200

    Add Stanford CoreNLP Server

---
+++ stanford-corenlp-server/src/main/java/pl/edu/agh/ed/twitter3/Server.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.ed.twitter3;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLPServer;
+
+import java.io.IOException;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        StanfordCoreNLPServer.main(args);
+    }
+}
